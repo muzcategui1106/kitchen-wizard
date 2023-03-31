31a6d74d071a16face194c793b962cae5900c496
muzcategui-1106 <muzcategui1106@gmail.com>
2023-03-31 13:36:06 -0400

    ingredient list view with images

--- frontend/src/Ingredients/AddIngredientForm.js
+++ frontend/src/Ingredients/AddIngredientForm.js
@@ -1,88 +1,118 @@
 import React, { useState } from 'react';
-import { Button, Modal, Form, Container } from 'react-bootstrap';
+import { Button, Modal, Form, Container, Alert  } from 'react-bootstrap';
+import axios from 'axios';
 
 export function IngredientForm(props) {
-    const [show, setShow] = useState(false);
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState(null);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    const handleNameChange = (event) => {
-      setName(event.target.value);
-    }
-  
-    const handleDescriptionChange = (event) => {
-      setDescription(event.target.value);
-    }
-  
-    const handleImageChange = (event) => {
-      setImage(event.target.files[0]);
-    }
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      
-      // Create a new FormData object and add the form data to it
-      const formData = new FormData();
-      formData.append('name', name);
-      formData.append('description', description);
-      formData.append('image', image);
-  
-      // Send a REST API request to your service
-      fetch('https://your-service.com/api/ingredients', {
-        method: 'POST',
-        body: formData
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setShow(false);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }
-  
-    return (
-      <Container>
-        <Button variant="primary"  onClick={handleShow}>
-          Add Ingredient
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Ingredient</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formIngredientName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter ingredient name" value={name} onChange={handleNameChange} />
-              </Form.Group>
-  
-              <Form.Group controlId="formIngredientDescription">
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Enter ingredient description" value={description} onChange={handleDescriptionChange} />
-              </Form.Group>
-  
-              <Form.Group controlId="formIngredientImage">
-                <Form.Label>Image</Form.Label>
-                <Form.Control type="file" accept="image/*" onChange={handleImageChange} />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="primary" onClick={handleSubmit}>
-              Save
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Container>
-    );
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
+  const [apiErrorMessage, setApiErrorMessage] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
   }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  }
+
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Read the file as a binary buffer
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(image);
+
+    fileReader.onload = async () => {
+      const imageBytes = new Uint8Array(fileReader.result);
+
+
+      console.log(imageBytes)
+      const data = {
+        ingredient: {
+          name: name,
+          description: description
+        },
+        image: Array.from(imageBytes)
+
+      };
+
+      const options = {
+        url: 'http://localhost:8443/api/v1/ingredient',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        },
+        data: JSON.stringify(data),
+        withCredentials: true
+      };
+
+      axios(options)
+        .then((response) => {
+          if (response.status == 200 ) {
+            setShow(false); // close the modal form
+          } else {
+            console.log(`statusCode: ${response.status}`);
+            console.log(response.data);
+            setApiErrorMessage(response.data.error)
+          }
+        })
+        .catch((error) => {
+          setApiErrorMessage(JSON.stringify(error.response.data.error));
+          console.error(error);
+        });
+    };
+
+
+  }
+
+  return (
+    <Container>
+      <Button variant="primary" onClick={handleShow}>
+        Add Ingredient
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Ingredient</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formIngredientName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter ingredient name" value={name} onChange={handleNameChange} />
+            </Form.Group>
+
+            <Form.Group controlId="formIngredientDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control type="text" placeholder="Enter ingredient description" value={description} onChange={handleDescriptionChange} />
+            </Form.Group>
+
+            <Form.Group controlId="formIngredientImage">
+              <Form.Label>Image</Form.Label>
+              <Form.Control type="file" accept="image/*" onChange={handleImageChange} />
+            </Form.Group>
+          </Form>
+          {apiErrorMessage && <Alert variant="danger" className="mt-3">{apiErrorMessage}</Alert>}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+}
--- frontend/src/Ingredients/IngredientList.js
+++ frontend/src/Ingredients/IngredientList.js
@@ -2,17 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Image, Card } from 'react-bootstrap';
 import { IngredientForm } from './AddIngredientForm'
 
-export function IngredientListPanel(){
+export function IngredientListPanel() {
   return (
     <Container>
-      <IngredientForm/>
-      <IngredientList/>
+      <IngredientForm />
+      <IngredientList />
     </Container>
   )
 }
 
 function IngredientList() {
   const [ingredients, setIngredients] = useState([]);
+
+
+  async function downloadImage(imageUrl) {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      return url;
+    } catch (error) {
+      console.error('Error downloading image:', error);
+      throw error;
+    }
+  }
 
   useEffect(() => {
     const headers = new Headers();
@@ -23,24 +36,40 @@ function IngredientList() {
       credentials: 'include'
     })
       .then(response => response.json())
-      .then(data => setIngredients(data));
-  }, []);
+      .then(data => {
+        // For each ingredient, download its image and set its URL in state
+        const promises = data.map(async (ingredient) => {
+          const imageUrl = `http://kitchen-wizard.store.s3.local.uzcatm-skylab.com/kitchen-wizard/ingredients/${ingredient.name}.jpg`;
+          const url = await downloadImage(imageUrl);
+          return {
+            ...ingredient,
+            imageUrl: url
+          };
+        });
 
-    return (
-      <Container>
-        <Row>
-          {ingredients.map((ingredient, index) => (
-            <Col md={4} key={index} className="my-3">
-              <Card>
-                <Image src={ingredient.imageSrc} fluid />
-                <Card.Body>
-                  <Card.Title>{ingredient.name}</Card.Title>
-                  <Card.Text>{ingredient.description}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
-  };
+        Promise.all(promises).then((data) => setIngredients(data));
+      });
+    }, []);
+
+  return (
+    <Container>
+      <Row>
+        {ingredients.map((ingredient, index) => (
+          <Col md={4} key={index} className="my-3">
+            <Card>
+              <Image
+                src={ingredient.imageUrl}
+                alt={ingredient.name}
+                fluid
+              />
+              <Card.Body>
+                <Card.Title>{ingredient.name}</Card.Title>
+                <Card.Text>{ingredient.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
